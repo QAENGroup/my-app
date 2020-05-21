ddba30681df5b7c8303a19a5b6f726f0c1cff5cd
dkanashov <dkanashov@gmail.com>
2020-05-21 16:30:06 +0500

    34-yrok

--- src/redux/state.js
+++ src/redux/state.js
@@ -8,7 +8,8 @@ let state = {
         {id: 2, message: 'It\'s my first post', likesCount: 11},
         {id: 3, message: 'Blabla', likesCount: 11},
         {id: 4, message: 'Dada', likesCount: 11}
-    ]},
+    ],
+    newPostText: 'it-kamasutra.com'},
     dialogsPage:{
         dialogs : [
             {id: 1, name: 'Dimych'},
@@ -28,14 +29,21 @@ let state = {
     sidebar:{}
 }
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
         id:5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount:0
     }
         state.profilePage.posts.push(newPost)
+        state.profilePage.newPostText = ''
         rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
 }
 
 export default state
