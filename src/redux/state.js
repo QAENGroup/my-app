199df02764d375053d2ce817229861c9d67371e4
dkanashov <dkanashov@gmail.com>
2020-06-14 16:17:31 +0500

    39

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST'
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+
 let store = {
     _state: {
             profilePage:{
@@ -54,7 +57,7 @@ let store = {
     },
 
     dispatch(action) { // {type: 'ADD-POST' }
-        if (action.type === 'ADD-POST'){
+        if (action.type === ADD_POST){
             //this._addPost()
             let newPost = {
                 id:5,
@@ -65,13 +68,20 @@ let store = {
             this._state.profilePage.newPostText = ''
             this._callSubscriber(this._state);
         }
-        else if (action.type === 'UPDATE-NEW-POST-TEXT'){
+        else if (action.type === UPDATE_NEW_POST_TEXT){
            // this._updateNewPostText(action.newText)
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber (this._state);
         }
     }
+
+
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+
+export const updateNewPostTextActionCreator = (text) =>
+    ({ type: UPDATE_NEW_POST_TEXT, newText:text})
 
 export default store
 window.store = store;
